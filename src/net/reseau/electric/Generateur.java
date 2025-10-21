52dad04bc2eaf520ba4b5e867183a8f2c3aa2453
amydiallo295 <amydiallo295@gmail.com>
2025-10-21 15:25:07 +0200

    Creation de la classe generateur

---
+++ src/net/reseau/electric/Generateur.java
@@ -0,0 +1,13 @@
+public class Generateur {
+    private final String nom;
+    private final int capacite;
+
+    public Generateur(String nom, int capacite){
+        this.nom = nom;
+        this.capacite = capacite;
+    }
+
+    public int getCapacite(){
+        return capacite;
+    }
+}
