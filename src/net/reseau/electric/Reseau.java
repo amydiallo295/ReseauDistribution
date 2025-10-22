b49ac4064c2eb7489cca6f9378533b21d314d840
Elodie Cao <yennhi@MacBook-Air-de-Yen.local>
2025-10-22 15:13:50 +0200

    Ajout des méthodes permettant d'enlever une connection

--- src/net/reseau/electric/Reseau.java
+++ src/net/reseau/electric/Reseau.java
@@ -7,6 +7,7 @@ public class Reseau{
     private Map<String, String> connections = new HashMap<>();
 
     public void ajouterGenerateur(String nomGenerateur, int capacite){
+    /* Méthode permettant d'ajouter des générateurs dans la liste des générateurs */
         if(generateurs.containsKey(nomGenerateur)){
             System.out.println("MAJ : Générateur " + nomGenerateur + " mise à jour");
         }
@@ -14,6 +15,7 @@ public class Reseau{
     }
 
     public void ajouterMaison(String nomMaison, TypeMaison type){
+    /* Méthode permettant d'ajouter des maisons dans la liste des maisons  */
         if(maisons.containsKey(nomMaison)){
             System.out.println("MAJ : Maison " + nomMaison + " mise à jour");
         }
@@ -21,8 +23,14 @@ public class Reseau{
     }
 
     public void ajouterConnections(String nomMaison, String nomGenerateur){
+    /* Méthode permettant d'ajouter des connections dans la liste des connections */
         if(! maisons.containsKey(nomMaison) || ! generateurs.containsKey(nomGenerateur))
             throw new IllegalArgumentException("Erreur de type ! Veuillez choisir entre : BASSE, NORMAL, HAUTE");
         connections.put(nomMaison, nomGenerateur);
+    }
+
+    public void enleverConnection(String nomMaison){
+    /* Méthode permettant de supprimer une connection */
+        connections.remove(nomMaison);
     }
 }
