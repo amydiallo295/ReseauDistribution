f9c0d4c968bd52da96151e32507719b6c28975fa
Elodie Cao <yennhi@MacBook-Air-de-Yen.local>
2025-10-22 15:08:39 +0200

    Ajout des méthodes permettant d'ajouter des générateurs, des maisons et des connections

--- src/net/reseau/electric/Reseau.java
+++ src/net/reseau/electric/Reseau.java
@@ -5,4 +5,24 @@ public class Reseau{
     private Map<String, Generateur> generateurs = new HashMap<>();
     private Map<String, Maison> maisons = new HashMap<>();
     private Map<String, String> connections = new HashMap<>();
+
+    public void ajouterGenerateur(String nomGenerateur, int capacite){
+        if(generateurs.containsKey(nomGenerateur)){
+            System.out.println("MAJ : Générateur " + nomGenerateur + " mise à jour");
+        }
+        generateurs.put(nomGenerateur, new Generateur(nomGenerateur, capacite));
+    }
+
+    public void ajouterMaison(String nomMaison, TypeMaison type){
+        if(maisons.containsKey(nomMaison)){
+            System.out.println("MAJ : Maison " + nomMaison + " mise à jour");
+        }
+        maisons.put(nomMaison, new Maison(nomMaison, type));
+    }
+
+    public void ajouterConnections(String nomMaison, String nomGenerateur){
+        if(! maisons.containsKey(nomMaison) || ! generateurs.containsKey(nomGenerateur))
+            throw new IllegalArgumentException("Erreur de type ! Veuillez choisir entre : BASSE, NORMAL, HAUTE");
+        connections.put(nomMaison, nomGenerateur);
+    }
 }
