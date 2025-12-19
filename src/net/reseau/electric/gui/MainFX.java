f1e1647971b2b470e620d6761706c06bda82886e
amydiallo295 <amydiallo295@gmail.com>
2025-12-19 11:41:48 +0100

    done

---
+++ src/net/reseau/electric/gui/MainFX.java
@@ -0,0 +1,22 @@
+package net.reseau.electric.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainFX extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/net/reseau/electric/view/main.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Réseau Électrique");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
